bf6aa7c61d34ebfe7e9610a5562d1a9c17cdd8ba
lewisle <phuonghoangle@gmail.com>
2016-01-01 18:44:15 +0700

    Apply babel to transform scripts folder

--- app/scripts/contextmenus.js
+++ app/scripts/contextmenus.js
@@ -3,8 +3,8 @@
 chrome.contextMenus.create({
   "title": chrome.i18n.getMessage("contextMenuTitle"),
   "contexts": ["selection"],
-  "onclick": function (info, tab) {
-    var selectionText = info.selectionText;
-    window.open('http://www.oxfordlearnersdictionaries.com/definition/english/' + selectionText + '?q=' + selectionText, '_blank');
+  "onclick": (info, tab) => {
+    const selectionText = info.selectionText
+    window.open(`http://www.oxfordlearnersdictionaries.com/definition/english/${selectionText}?q=${selectionText}`, '_blank');
   }
-});
+})
