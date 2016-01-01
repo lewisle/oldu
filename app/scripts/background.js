1db9b064d1fa55fb5def1d16e28c43bdd1329f87
lewisle <phuonghoangle@gmail.com>
2016-01-01 11:35:05 +0700

    Init options and popup

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,10 +1,9 @@
 'use strict';
 
 chrome.runtime.onInstalled.addListener(function (details) {
-  console.log('previousVersion', details.previousVersion);
+  //console.log('previousVersion', details.previousVersion);
 });
 
-chrome.browserAction.setBadgeText({ text: '\'Allo' });
-
-console.log('\'Allo \'Allo! Event Page for Browser Action');
+// chrome.browserAction.setBadgeText({ text: 'oldu' });
+// console.log('\'Allo \'Allo! Event Page for Browser Action');
 //# sourceMappingURL=background.js.map
--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,4 +1,1 @@
 'use strict';
-
-console.log('\'Allo \'Allo! Popup');
-//# sourceMappingURL=popup.js.map
